29f05b707e078c2f554c8d6451fbeba3920b29ea
cryptoclans <contact@cryptoclans.com>
2023-01-19 20:14:04 +0000

    Add wallet logo
    add wallet logo

--- src/components/navbar/WalletPopover.tsx
+++ src/components/navbar/WalletPopover.tsx
@@ -4,7 +4,10 @@ import { Popover, Transition } from '@headlessui/react';
 import { useChain } from '@cosmos-kit/react';
 import Link from 'next/link';
 
+// TODO do this with the options below
+// find out the right way to the chainsList
 const chainsList = [{ name: 'ATOM' }, { name: 'OSMO' }, { name: 'JUNO' }];
+import { GasPrice } from '@cosmjs/stargate';
 
 export default function WalletPopover() {
   const chainContext = useChain('juno');
@@ -84,14 +87,18 @@ export default function WalletPopover() {
             <Popover.Panel className='absolute left-1/2 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-2 sm:px-0'>
               <div className='overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5'>
                 <div className='relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8'>
-                  <p className='text-base font-medium text-gray-900'>
-                    {chain.chain_name}
+                  <p className='flex justify-center text-base font-medium text-gray-900'>
+                    <img
+                      className='h-20 w-20 rounded-full '
+                      src={logoUrl}
+                      alt=''
+                    />
                   </p>
                   <p className='mt-1 text-sm text-gray-500'>
                     <form>
                       <label className='relative block'>
                         <input
-                          className='placeholder:font-italitc w-full rounded-md border border-slate-400 bg-white py-2 pl-3 pr-10 drop-shadow-md focus:outline-none'
+                          className='placeholder:font-italitc w-full rounded-md border border-slate-400 bg-white py-2 pl-3 pr-10 focus:outline-none'
                           value={address?.slice(0, 20).concat('...')}
                           type='text'
                           disabled
@@ -117,6 +124,24 @@ export default function WalletPopover() {
                       </label>
                     </form>
                   </p>
+
+                  <select
+                    id='large'
+                    className='block w-full rounded-lg border border-gray-300 bg-gray-50 px-4 py-3 text-base text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
+                  >
+                    <option defaultValue={chain.chain_id}>
+                      {chain.chain_name
+                        .charAt(0)
+                        .toUpperCase()
+                        .concat(
+                          chain.chain_name.substring(1, chain.chain_name.length)
+                        )}
+                    </option>
+                    <option value='US'>United States</option>
+                    <option value='CA'>Canada</option>
+                    <option value='FR'>France</option>
+                    <option value='DE'>Germany</option>
+                  </select>
 
                   <Link href={''} onClick={() => disconnect()}>
                     <button className='w-full rounded bg-blue-500 py-2 px-4 text-center font-bold text-white hover:bg-blue-700'>
