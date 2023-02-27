134a08d4ec46aa880ae97d96663a071eba0150a0
ysw421 <ysw421@naver.com>
2023-02-28 02:38:30 +0900

    a.m. 2:38

---
+++ src/parameters/Papers/Pages/Page4.jsx
@@ -0,0 +1,14 @@
+import styles from './Pages.module.css';
+import { MainText, Height50 } from './useful';
+
+export default function Page1(props) {
+  return (
+    <>
+      <MainText text="한국디지털미디어고등학교(Korea Digital Media High School, KDMHS) 21기 웹 프로그래밍과 윤시원입니다. :)" />
+      <Height50 num="120px" />
+      <MainText text="21기: 2022년 3월 ~" />
+      <Height50 num="20px" />
+      <MainText text="인공지능 동아리 Fregic 12기" />
+    </>
+  );
+}
--- src/parameters/Papers/Papers.jsx
+++ src/parameters/Papers/Papers.jsx
@@ -1,6 +1,7 @@
 import Page1 from './Pages/Page1';
 import Page2 from './Pages/Page2';
 import Page3 from './Pages/Page3';
+import Page4 from './Pages/Page4';
 
 function Papers(props) {
   return (
@@ -8,6 +9,7 @@ function Papers(props) {
       {props.numberId === 1 && <Page1 isDarkMode={props.isDarkMode} />}
       {props.numberId === 2 && <Page2 isDarkMode={props.isDarkMode} />}
       {props.numberId === 3 && <Page3 isDarkMode={props.isDarkMode} />}
+      {props.numberId === 4 && <Page4 isDarkMode={props.isDarkMode} />}
       <div style={{ height: '50px' }}></div>
     </div>
   );
