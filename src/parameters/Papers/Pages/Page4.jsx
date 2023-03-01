7ff2926b6a5edde82d3b69c137ac09c05b4b283b
SiWon Yun <63529804+ysw421@users.noreply.github.com>
2023-03-01 12:59:25 +0900

    pull request (#4)
    * add link
    
    * Update issue templates (#1)

--- src/parameters/Papers/Pages/Page4.jsx
+++ src/parameters/Papers/Pages/Page4.jsx
@@ -2,9 +2,22 @@ import styles from './Pages.module.css';
 import { MainText, Height50 } from './useful';
 
 export default function Page1(props) {
+  const handleOpenNewTab = (url) => {
+    window.open(url, '_blank', 'noopener, noreferrer');
+  };
+
   return (
     <>
-      <MainText text="한국디지털미디어고등학교(Korea Digital Media High School, KDMHS) 21기 웹 프로그래밍과 윤시원입니다. :)" />
+      <span
+        style={{ textDecoration: 'underline', cursor: 'pointer' }}
+        onClick={() => handleOpenNewTab('https://www.dimigo.hs.kr/')}
+        className={styles.mainText}
+      >
+        한국디지털미디어고등학교
+      </span>
+      <span className={styles.mainText}>
+        (Korea Digital Media High School, KDMHS) 21기 웹 프로그래밍과 윤시원입니다. :)
+      </span>
       <Height50 num="120px" />
       <MainText text="21기: 2022년 3월 ~" />
       <Height50 num="20px" />
