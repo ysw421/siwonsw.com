630adf29df05184a80c7c4f3a00b2a630db3a1d7
ysw421 <ysw421@naver.com>
2023-03-04 22:57:15 +0900

    write page

--- src/parameters/Papers/Pages/Page8.jsx
+++ src/parameters/Papers/Pages/Page8.jsx
@@ -1,5 +1,34 @@
 import { MainText, Height50, Line, Box } from './useful';
+import { MathJaxContext, MathJax } from 'better-react-mathjax';
 
 export default function Page8(props) {
-  return <></>;
+  const config = {
+    loader: { load: ['[tex]/html', 'output/svg'] },
+    tex: {
+      packages: { '[+]': ['html'] },
+      inlineMath: [
+        ['$', '$'],
+        ['\\(', '\\)'],
+      ],
+      displayMath: [
+        ['$$', '$$'],
+        ['\\[', '\\]'],
+      ],
+    },
+  };
+
+  return (
+    <>
+      <MathJaxContext version={3} config={config}>
+        <MainText text="행렬은 숫자를 직사각형 형태로 배열한 것입니다." />
+        <MainText text="(m, n) 형태의 행렬은 다음과 같이 나타낼 수 있습니다." />
+        <MathJax>{'\\((m, n)\\in\\mathbb{N}\\)'}</MathJax>
+        <MathJax>
+          {
+            '\\(\\begin{bmatrix}a_{11} & a_{12} & \\cdots & a_{1n}\\\\a_{21} & a_{22} & \\cdots & a_{2n}\\\\\\vdots & \\vdots & \\ddots & \\vdots\\\\a_{m1} & a_{m2} & \\cdots & a_{mn}\\end{bmatrix}\\)'
+          }
+        </MathJax>
+      </MathJaxContext>
+    </>
+  );
 }
