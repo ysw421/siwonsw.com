0c7bebc969194afc6d0c0c00154cc6d61296e9b7
SiWon Yun <63529804+ysw421@users.noreply.github.com>
2023-10-10 21:29:15 +0900

    fixed build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests',
+          },
+        ],
+      },
+    ]
+  },
+
+  images: {
+    unoptimized: true,
+  },
+  
   eslint: {
     dirs: ['src'],
   },
