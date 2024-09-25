ef808b7a67fb09969880dc962f1a654a663a8e43
ysw421 <ysw421@naver.com>
2024-09-25 09:44:36 +0900

    add Google Analytics

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -8,6 +8,7 @@ import scrollBar from '@/lib/scrollBar.module.scss';
 
 import { isDarkMode_ } from '@/lib/darkMode';
 
+import GoogleAnalytics from '@/components/layout/gtag';
 import Header from '@/components/layout/Header';
 import MindMapLayout from '@/components/layout/MindMapLayout';
 import SetModeBtn from '@/components/SetModeBtn';
@@ -36,44 +37,54 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   if (folder[1] === 'mind-map') {
     return (
-      <MindMapLayout isMd={isMd} isAllowAnimation={isAllowAnimation}>
-        {children}
-      </MindMapLayout>
+      <>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
+        <MindMapLayout isMd={isMd} isAllowAnimation={isAllowAnimation}>
+          {children}
+        </MindMapLayout>
+      </>
     );
   } else {
     return (
-      <div
-        className={`relative h-screen w-screen overflow-hidden ${
-          isDarkMode ? 'dark' : ''
-        }`}
-      >
-        <style jsx global>{`
-          body {
-            background: ${isDarkMode ? 'bg-dark' : 'bg-light'};
-          }
-        `}</style>
+      <>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
+        <div
+          className={`relative h-screen w-screen overflow-hidden ${
+            isDarkMode ? 'dark' : ''
+          }`}
+        >
+          <style jsx global>{`
+            body {
+              background: ${isDarkMode ? 'bg-dark' : 'bg-light'};
+            }
+          `}</style>
 
-        {/* <span>{mode}</span> */}
-        {/* <span className='hidden' key={isDarkMode ? 1 : 2}>
+          {/* <span>{mode}</span> */}
+          {/* <span className='hidden' key={isDarkMode ? 1 : 2}>
           {isDarkMode === true ? '11' : '22'}
         </span> */}
-        {/* <div className='fixed top-0 left-0 z-50 w-full'></div> */}
-        <div
-          className={`relative flex h-full w-full flex-col overflow-y-auto bg-light text-dark dark:bg-dark dark:text-light ${
-            scrollBar.scrollBar
-          } ${isAllowAnimation && styles.transition}`}
-        >
-          <div className='sticky top-0 left-0 z-50 h-[68px] w-full'>
-            <Header isAllowAnimation={isAllowAnimation} />
-          </div>
-          <div className='h-full w-full'>{children}</div>
-          <div className='fixed right-3 bottom-3 flex flex-col items-end gap-2'>
-            <div className='flex items-center gap-2 rounded-md bg-[#f8f8f880] p-2 px-5 backdrop-blur-[1px] dark:bg-[#282c3580]'>
-              <SetModeBtn type={isMd ? 1 : 2} />
+          {/* <div className='fixed top-0 left-0 z-50 w-full'></div> */}
+          <div
+            className={`relative flex h-full w-full flex-col overflow-y-auto bg-light text-dark dark:bg-dark dark:text-light ${
+              scrollBar.scrollBar
+            } ${isAllowAnimation && styles.transition}`}
+          >
+            <div className='sticky top-0 left-0 z-50 h-[68px] w-full'>
+              <Header isAllowAnimation={isAllowAnimation} />
+            </div>
+            <div className='w-full h-full'>{children}</div>
+            <div className='fixed flex flex-col items-end gap-2 right-3 bottom-3'>
+              <div className='flex items-center gap-2 rounded-md bg-[#f8f8f880] p-2 px-5 backdrop-blur-[1px] dark:bg-[#282c3580]'>
+                <SetModeBtn type={isMd ? 1 : 2} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
